ee54e2153a3b94a979446eb56bb90868078b8ed9
Diana7e <99027242+Diana7e@users.noreply.github.com>
2022-03-26 16:05:50 -0400

    submission

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -33,6 +33,7 @@ FROM unique_titles as ut
 GROUP BY title 
 ORDER BY COUNT(title) DESC;
 
+SELECT * FROM retiring_titles;
 -- DELIVERABLE 2: The Employees Eligible for the Mentorship Program
 
 
@@ -53,3 +54,26 @@ ON (e.emp_no = ti.emp_no)
 WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31') AND (de.to_date  = '9999-01-01')  
 ORDER BY e.emp_no ASC, de.to_date DESC;
 
+
+SELECT * FROM mentorship_eligibilty;
+
+
+
+--delivarable 3
+SELECT DISTINCT ON (emp_no) e.emp_no, d.dept_name, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.title
+INTO employees_leaving_by_dept
+FROM employees as e
+JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+JOIN titles as t
+ON (e.emp_no = t.emp_no)
+LEFT JOIN departments as d
+ON (de.dept_no = d.dept_no)
+WHERE (de.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1962-01-01' AND '1965-12-31')
+    AND (de.from_date BETWEEN '1985-01-01' AND '1988-12-31')
+ORDER BY e.emp_no ASC;
+
+SELECT COUNT(first_name) "Count", dept_name
+FROM employees_leaving_by_dept
+GROUP BY dept_name
+ORDER BY "Count" desc;
